afcc202ecf314a41a87b2a2672c3799a9a490a3c
HacKan <hackan@gmail.com>
2019-06-29 22:25:51 -0300

    Bump version

--- secretshare/__init__.py
+++ secretshare/__init__.py
@@ -5,6 +5,6 @@ Implements Shamir's secret sharing algorithm.
 
 from .secretshare import Secret, SecretShare, Share
 
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 
 __all__ = ('Secret', 'SecretShare', 'Share', '__version__')
